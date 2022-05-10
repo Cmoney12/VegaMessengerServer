e66b1572739039f3621b76d2b905ca70214d8edf
corey <coreylovette@gmail.com>
2022-05-09 19:19:04 -0500

    Initial commit

---
+++ Serialization.h
@@ -0,0 +1,169 @@
+//
+// Created by corey on 2/27/22.
+//
+
+#ifndef VEGAMESSENGERSERVER_SERIALIZATION_H
+#define VEGAMESSENGERSERVER_SERIALIZATION_H
+
+#include <bson.h>
+
+struct Protocol {
+    int32_t status_code;
+    char receivers_number[11];
+    char senders_number[11];
+    char data[30];
+    char port[6];
+};
+
+class Serialization {
+public:
+    enum { HEADER_LENGTH = 4 };
+
+    Serialization():body_length_(0){}
+
+    std::uint8_t* data() const
+    {
+        return data_.get();
+    }
+
+    const std::uint8_t* data()
+    {
+        return data_.get();
+    }
+
+    std::uint8_t* head() {
+        return header;
+    }
+
+    std::size_t length() const
+    {
+        return HEADER_LENGTH + body_length_;
+    }
+
+    std::uint8_t* body()
+    {
+        return data_.get() + HEADER_LENGTH;
+    }
+
+    int body_length() const
+    {
+        return body_length_;
+    }
+
+    void set_size(const std::size_t& size) {
+        body_length_ = (int)size;
+        data_ = std::make_unique<std::uint8_t[]>(size + HEADER_LENGTH);
+    }
+
+    const char* parse_bson(const std::uint8_t *bson_data, std::size_t size, const std::string& ip_address) {
+
+        bson_t *received;
+        bson_iter_t iter;
+        uint32_t size1;
+        const char *receiver = nullptr;
+
+        received = bson_new_from_data(bson_data, size);
+
+        if (bson_iter_init_find(&iter, received, "Receivers_Number") && BSON_ITER_HOLDS_UTF8(&iter)) {
+            receiver = bson_iter_utf8(&iter, nullptr);
+        }
+
+        // If we receive an accept we create a new bson to append the ip address of the sender to data
+        // portion of the packet
+        if (bson_iter_init_find(&iter, received, "Status_Code") && BSON_ITER_HOLDS_INT32(&iter)) {
+            int32_t status_code = bson_iter_int32(&iter);
+            if (status_code == 200 || status_code == 100) {
+                Protocol protocol{};
+
+                std::memcpy(protocol.receivers_number, receiver, std::strlen(receiver));
+                if (bson_iter_init_find(&iter, received, "Status_Code") && BSON_ITER_HOLDS_INT32(&iter)) {
+                    protocol.status_code = bson_iter_int32(&iter);
+                }
+
+                if (bson_iter_init_find(&iter, received, "Senders_Number") && BSON_ITER_HOLDS_UTF8(&iter)) {
+                    const char *deliverer = bson_iter_utf8(&iter, nullptr);
+                    std::memcpy(protocol.senders_number, deliverer, std::strlen(deliverer));
+                }
+
+                if (bson_iter_init_find(&iter, received, "Port") && BSON_ITER_HOLDS_UTF8(&iter)) {
+                    const char* receiver_port = bson_iter_utf8(&iter, nullptr);
+                    std::memcpy(protocol.port, receiver_port, std::strlen(receiver_port));
+                }
+
+                data_.release();
+                create_bson(protocol, ip_address.c_str());
+                //std::cout << ip_address << std::endl;
+
+                return receiver;
+
+            }
+        }
+
+        bson_destroy(received);
+
+        return receiver;
+    }
+
+
+    void create_bson(const Protocol& protocol, const char* ip_address) {
+        bson_t document{};
+        bson_init(&document);
+        const uint8_t *bson;
+
+        bson_append_int32(&document, "Status_Code", -1, protocol.status_code);
+        bson_append_utf8(&document, "Receivers_Number", -1, protocol.receivers_number, -1);
+        bson_append_utf8(&document, "Senders_Number", -1, protocol.senders_number, -1);
+        bson_append_utf8(&document, "Data", -1, ip_address, -1);
+        bson_append_utf8(&document, "Port", -1, protocol.port, -1);
+        body_length_ = (int)document.len;
+
+        bool steal = true;
+        uint32_t size;
+
+        bson = bson_destroy_with_steal(&document, steal, &size);
+
+        body_length_ = (int)document.len;
+        data_ = std::make_unique<uint8_t[]>(body_length_ + HEADER_LENGTH);
+        encode_header();
+        std::memcpy(data_.get() + HEADER_LENGTH, bson, body_length_);
+
+    }
+
+    bool decode_header() {
+        std::memcpy(&body_length_, header, sizeof body_length_);
+        set_size(body_length_);
+        //std::memcpy(data_, header, HEADER_LENGTH);
+        data_.get()[3] = (body_length_>>24) & 0xFF;
+        data_.get()[2] = (body_length_>>16) & 0xFF;
+        data_.get()[1] = (body_length_>>8) & 0xFF;
+        data_.get()[0] = body_length_ & 0xFF;
+        if(body_length_ > MAX_MESSAGE_SIZE) {
+            body_length_ = 0;
+            return false;
+        }
+        return true;
+    }
+
+    bool encode_header() const {
+        if (body_length_ <= MAX_MESSAGE_SIZE && body_length_) {
+            data_[3] = (body_length_>>24) & 0xFF;
+            data_[2] = (body_length_>>16) & 0xFF;
+            data_[1] = (body_length_>>8) & 0xFF;
+            data_[0] = body_length_ & 0xFF;
+            return true;
+        }
+        return false;
+    }
+
+
+private:
+    int body_length_{};
+    std::unique_ptr<uint8_t[]> data_{};
+    enum { MAX_MESSAGE_SIZE = 99999999 };
+    uint8_t header[HEADER_LENGTH]{};
+
+private:
+
+};
+
+#endif //VEGAMESSENGERSERVER_SERIALIZATION_H
---
+++ main.cpp
@@ -0,0 +1,283 @@
+#include <iostream>
+
+#include <cstdlib>
+#include <deque>
+#include <list>
+#include <memory>
+#include <set>
+#include <utility>
+#include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
+#include <unordered_map>
+#include "Serialization.h"
+
+using boost::asio::ip::tcp;
+
+//----------------------------------------------------------------------
+
+typedef std::deque<std::shared_ptr<Serialization>> chat_message_queue;
+
+//----------------------------------------------------------------------
+
+class chat_participant
+{
+public:
+    virtual ~chat_participant() = default;
+    virtual void deliver(const std::string& recipient, const std::shared_ptr<Serialization>& msg) = 0;
+};
+
+typedef std::shared_ptr<chat_participant> chat_participant_ptr;
+
+//----------------------------------------------------------------------
+
+class chat_room {
+public:
+
+    void join(const std::string& username, const chat_participant_ptr& participant)
+    {
+        participants_.emplace(username, participant);
+        //for (const auto& msg: recent_msgs_)
+        //    participant->deliver(msg);
+    }
+
+    void leave(const chat_participant_ptr& participant)
+    {
+        for (auto it = participants_.begin(); it != participants_.end(); )
+        {
+            if (it->second == participant) {
+                participants_.erase(it++);
+            }
+            else {
+                ++it;
+            }
+        }
+
+    }
+
+    void deliver(const std::string& recipient, const std::shared_ptr<Serialization>& msg)
+    {
+        if (participants_.find(recipient) != participants_.end()) {
+            recent_msgs_.push_back(msg);
+            while (recent_msgs_.size() > max_recent_msgs)
+                recent_msgs_.pop_front();
+
+            auto it = participants_.find(recipient);
+            it->second->deliver(recipient, msg);
+        }
+    }
+
+private:
+    std::unordered_map<std::string, chat_participant_ptr> participants_;
+    enum { max_recent_msgs = 100 };
+    chat_message_queue recent_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_session
+        : public chat_participant,
+          public std::enable_shared_from_this<chat_session>
+{
+public:
+    chat_session(tcp::socket socket, chat_room& room, boost::asio::io_context::strand& strand)
+            : socket_(std::move(socket)),
+              room_(room),
+              strand_(strand)
+    {
+    }
+
+    void start()
+    {
+        read_username();
+        do_read_header();
+
+    }
+
+    void deliver(const std::string& recipient, const std::shared_ptr<Serialization>& msg) override
+    {
+        bool write_in_progress = !write_msgs_.empty();
+        write_msgs_.push_back(msg);
+        if (!write_in_progress)
+        {
+            do_write();
+        }
+    }
+
+    void read_username() {
+        boost::asio::async_read_until(socket_, buf, "\n",
+                                      boost::asio::bind_executor(strand_,[this]
+                                              (boost::system::error_code ec, std::size_t size) {
+                                          if(!ec) {
+                                              handle_username(ec, size);
+                                          }
+                                      }));
+    }
+
+    void handle_username(boost::system::error_code, std::size_t size) {
+        std::stringstream message;
+
+        message << std::istream(&buf).rdbuf();
+        buf.consume(size);
+        std::string username = message.str();
+        message.clear();
+        int pos = username.find('\n');
+        username = username.substr(0,pos);
+        room_.join(username, shared_from_this());
+    }
+
+private:
+    void do_read_header()
+    {
+        auto self(shared_from_this());
+        boost::asio::async_read(socket_,
+                                boost::asio::buffer(read_msg_->head(), Serialization::HEADER_LENGTH),
+                                boost::asio::bind_executor(strand_,[this, self](boost::system::error_code ec, std::size_t /*length*/)
+                                {
+                                    if (!ec && read_msg_->decode_header())
+                                    {
+                                        do_read_body();
+                                    }
+                                    else
+                                    {
+                                        room_.leave(shared_from_this());
+                                    }
+                                }));
+    }
+
+    void do_read_body()
+    {
+        auto self(shared_from_this());
+        boost::asio::async_read(socket_,
+                                boost::asio::buffer(read_msg_->body(), read_msg_->body_length()),
+                                boost::asio::bind_executor(strand_,[this, self](boost::system::error_code ec, std::size_t /*length*/)
+                                {
+                                    if (!ec) {
+                                        // allow through network to get external ip
+                                        std::string client_ip = socket_.remote_endpoint().address().to_string();
+                                        //local ip
+                                        std::string username = read_msg_->parse_bson(read_msg_->body(),
+                                                                                     read_msg_->body_length(), client_ip);
+                                        room_.deliver(username, read_msg_);
+                                        do_read_header();
+                                    }
+                                    else
+                                    {
+                                        room_.leave(shared_from_this());
+                                    }
+                                }));
+    }
+
+    void do_write()
+    {
+        auto self(shared_from_this());
+        boost::asio::async_write(socket_,
+                                 boost::asio::buffer(write_msgs_.front()->data(),
+                                                     write_msgs_.front()->length()),
+                                 boost::asio::bind_executor(strand_,
+                                                            [this, self](boost::system::error_code ec, std::size_t /*length*/)
+                                                            {
+                                                                if (!ec)
+                                                                {
+                                                                    write_msgs_.pop_front();
+                                                                    if (!write_msgs_.empty())
+                                                                    {
+                                                                        do_write();
+
+                                                                    }
+                                                                }
+                                                                else
+                                                                {
+                                                                    room_.leave(shared_from_this());
+                                                                }
+                                                            }));
+    }
+
+    tcp::socket socket_;
+    chat_room& room_;
+    std::shared_ptr<Serialization> read_msg_ = std::make_shared<Serialization>();
+    chat_message_queue write_msgs_;
+    boost::asio::streambuf buf;
+    boost::asio::io_context::strand& strand_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_server
+{
+public:
+    chat_server(boost::asio::io_context& io_context,
+                const tcp::endpoint& endpoint, boost::asio::io_context::strand& strand)
+            : acceptor_(io_context, endpoint), strand_(strand)
+    {
+        do_accept();
+    }
+
+private:
+    void do_accept()
+    {
+        acceptor_.async_accept(
+                boost::asio::bind_executor(strand_, [this](boost::system::error_code ec, tcp::socket socket)
+                {
+                    if (!ec)
+                    {
+                        std::make_shared<chat_session>(std::move(socket), room_, strand_)->start();
+                    }
+
+                    do_accept();
+                }));
+    }
+
+    tcp::acceptor acceptor_;
+    chat_room room_;
+    boost::asio::io_context::strand& strand_;
+};
+
+//----------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc < 2)
+        {
+            std::cerr << "Usage: chat_server <port> [<port> ...]\n";
+            return 1;
+        }
+        int port = 1234;
+        int thread_number = 3;
+        boost::asio::io_context io_context;
+        std::vector<std::thread> server_threads;
+        tcp::endpoint endpoint(tcp::v4(), port);
+        boost::asio::io_context::strand strand_ = boost::asio::io_service::strand(io_context);
+        std::shared_ptr<chat_server> server = std::make_shared<chat_server>(io_context, endpoint, strand_);
+
+        // Run the I/O service on the requested number of threads
+        boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+        signals.async_wait(
+                [&io_context](boost::system::error_code const&, int)
+                {
+                    // Stop the io_context. This will cause run()
+                    // to return immediately, eventually destroying the
+                    // io_context and any remaining handlers in it.
+                    io_context.stop();
+                });
+        // Run the I/O service on the requested number of threads
+        server_threads.reserve(thread_number);
+        for(auto i = thread_number - 1; i > 0; --i)
+            server_threads.emplace_back(
+                    [&io_context]
+                    {
+                        io_context.run();
+                    });
+        io_context.run();
+        // Block until all the threads exit
+        for (auto& i: server_threads)
+            i.join();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
